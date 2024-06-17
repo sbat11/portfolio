8b5734437dd7e2e20dcf52fb4cceef7e2740b88e
sbat11 <saransh.batwe518@gmail.com>
2024-06-17 16:22:03 -0400

    1 - auth controller added and added some packages

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,25 @@ import express from "express";
 import auth from "./routes/auth.js"
 import dotenv from "dotenv"
 import connectMongoDB from "./db/connectMongoDB.js";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 8000
 
 console.log(process.env.MONGO_URI);
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}))
+
+
+app.use(cookieParser());
+
 app.use("/api/auth",auth)
 
-app.listen(8000, () => {
+app.listen(PORT, () => {
     console.log("Server is running on port 8000");
     connectMongoDB();
 });
