9a118033075a4476ad8b65d673ef2d9689d65111
sbat11 <saransh.batwe518@gmail.com>
2024-07-17 10:59:58 -0400

    deployment

--- backend/server.js
+++ backend/server.js
@@ -32,6 +32,14 @@ app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
 
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    })
+}
+
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
 	connectMongoDB();
